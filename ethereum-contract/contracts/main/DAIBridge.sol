3d7b5d49c08991b312eb593e802664bb670faf9d
Shunpei Koike <shunpei42ba@gmail.com>
2019-10-31 13:45:58 +0900

    rename ui and adding ethereum-contract

---
+++ ethereum-contract/contracts/main/DAIBridge.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.8;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol';
+
+contract DAIBridge {
+    IERC20 private token;
+
+    event RelayMessage();
+
+    constructor (IERC20 _token) public {
+        token = _token;
+    }
+
+    function setTransfer(uint amount, bytes32 substrateAddress) public {
+        // TODO: validate
+        token.transferFrom(msg.sender, address(this), amount);
+        // TODO: event
+        emit RelayMessage();
+    }
+}
